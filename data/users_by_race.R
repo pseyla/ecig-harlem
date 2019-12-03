e7ff8b7bfff51fd39c0a799a8aa57d30979dcbe5
pseyla <phanseyla@gmail.com>
2019-12-02 22:03:16 -0500

    Adding all csv files used to collect data from 2018 Survey. Also adding sketches of percentage of smokers by race.

---
+++ data/users_by_race.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+users <- read.csv("../Percent_Smokers_by_Race.csv")
+
+
+
+p <- ggplot(
+            data = users,
+            mapping = aes(x = race, y = percent)
+)
+
+p + 
+  geom_col() + 
+  coord_flip()
+ 
